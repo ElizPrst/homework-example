8306a7b3e6b9cfcc2fc4665760a571f8e5871dbd
PopovaElis <elizprest@gmail.com>
2022-08-04 17:23:31 +0500

    задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 public class Main {
-   //задание 1
     public static void main(String[] args) {
-        int  clientOS = 0;
+                int ClientOS = 1;
+                int clientDeviceYear = 2014;
+        boolean os=(ClientOS==1);
+        boolean android=(ClientOS==0);
 
-        if (clientOS == 1) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }  else {
-            if (clientOS == 0) {
-            System.out.println("Установите версию приложения для Android по ссылке");
+                int Client = ; {
+                    if (clientDeviceYear > 2015)
+                        System.out.println("Установите версию приложения для iOS по ссылке");
+                    if (clientDeviceYear < 2015)  {
+                    }
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
-        }
+
+        int Client1 = ClientOS = 1; {
+                if (clientDeviceYear > 2015)
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                if (clientDeviceYear < 2015)  {
+                }
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                    }
+                }
     }
-}
-
-
-
