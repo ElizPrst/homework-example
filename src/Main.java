ffc486f275390955ee4902339ff93d5d2b25b51b
PopovaElis <elizprest@gmail.com>
2022-08-04 09:35:25 +0500

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+   //задание 1
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int  clientOS = 0;
+
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }  else {
+            if (clientOS == 0) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        }
     }
 }
+
+
+
